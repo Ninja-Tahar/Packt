482713387a8043e47421cd782c6f26130bae7e9f
Benkara Mohamed Tahar <tahar.benkara@ninjacoders-nc.com>
2020-01-14 20:26:59 +0200

    MD-01. Chapter2 Creating your first module.

---
+++ web/modules/custom/hello_world/src/EventSubscriber/HelloWorldRedirectSubscriber.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tahar
+ * Date: 14.01.2020
+ * Time: 18:19
+ */
+
+namespace Drupal\hello_world\EventSubscriber;
+
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Routing\LocalRedirectResponse;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Drupal\Core\Url;
+
+/**
+ * Subscribes to the Kernel Request event and redirects to the homepage
+ * when the user has the "non_grata" role.
+ */
+class HelloWorldRedirectSubscriber implements EventSubscriberInterface {
+
+  /**
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * HelloWorldRedirectSubscriber constructor.
+   *
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $currentRouteMatch
+   */
+  public function __construct(AccountProxyInterface $currentUser, CurrentRouteMatch $currentRouteMatch) {
+    $this->currentUser = $currentUser;
+    $this->currentRouteMatch = $currentRouteMatch;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[kernelEvents::REQUEST][] = ['onRequest', 0];
+    return $events;
+  }
+
+  /**
+   * Handler for the kernel request event.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   */
+  public function onRequest(GetResponseEvent $event) {
+    $route_name = $this->currentRouteMatch->getRouteName();
+
+    if ($route_name !== 'hello_world.hello') {
+      return;
+    }
+
+    $roles = $this->currentUser->getRoles();
+    if (in_array('non_grata', $roles)) {
+      $url = Url::fromUri('internal:/');
+      $event->setResponse(new LocalRedirectResponse($url->toString()));
+    }
+  }
+}
